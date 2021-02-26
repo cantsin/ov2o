1607120b19bf2c14760cc2ad1aadcd616a45f437
James Tranovich <jtranovich@gmail.com>
2021-02-25 21:49:10 -0800

    added shellHook

--- shell.nix
+++ shell.nix
@@ -2,7 +2,11 @@ with import <nixpkgs> { };
 stdenv.mkDerivation {
   name = "env";
   buildInputs = [ dune_2 ocaml opam gnum4 pkg-config ];
+  shellHook = ''
+    export UTOP_SITE_LISP=1
+    export MERLIN_SITE_LISP=1
+    export OCP_INDENT_SITE_LISP=1
+  '';
 }
+# opam install -y merlin ocamlformat core icalendar
 # eval $(opam env)
-# opam install -y core
-# opam install -y icalendar
