67a4b9b1047da241f44dae035e410c529a1f64d6
James Tranovich <jtranovich@gmail.com>
2021-07-09 17:21:34 -0700

    update env

--- opam2nix.nix
+++ opam2nix.nix
@@ -1,4 +1,4 @@
 import (builtins.fetchTarball {
   url = "https://github.com/timbertson/opam2nix/archive/v1.tar.gz";
-  sha256 = "1d60adr1z26mdn4hp146263f5fhb66yhwdfsf9m9lgpybbra7gkh";
+  sha256 = "1drw254961xqg2gz7pbkgpc81gxwhq11bzdp83kzc0sdj5hixsxd";
 })
